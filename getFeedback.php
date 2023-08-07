e2ce8b885a1bc340fd328bfc006bb4b8b26db114
Kaori <Kaori.Kumura@MyGeorgian.ca>
2023-08-07 15:24:43 -0400

    made some changes to feedback pages

--- getFeedback.php
+++ getFeedback.php
@@ -55,9 +55,9 @@
 	<div id="footer">
 		<div>
 			<div>
-				<a href="http://freewebsitetemplates.com/go/twitter/" target="_blank" id="twitter">Twitter</a>
-				<a href="http://freewebsitetemplates.com/go/facebook/" target="_blank" id="facebook">Facebook</a>
-				<a href="http://freewebsitetemplates.com/go/googleplus/" target="_blank" id="googleplus">Google&#43;</a>
+				<a href="https://twitter.com/" target="_blank" id="twitter">Twitter</a>
+				<a href="http://facebook.com/" target="_blank" id="facebook">Facebook</a>
+				<a href="http://googleplus.com/" target="_blank" id="googleplus">Google&#43;</a>
 			</div>
 			<p>
 				&copy; Copyright 2012. All rights reserved
